1561501a07a867bf639e4896dd57d67bc31b747e
Abhinesh005 <92208919+Abhinesh005@users.noreply.github.com>
2021-10-10 16:00:00 +0530

    Update Tutorial9.c
    amangwari021/hacktoberfest

--- Tutorial9.c
+++ Tutorial9.c
@@ -2,32 +2,25 @@
 
 int main ()
 {
-int age;
-printf("enter your age \n");
+    int age;
+    printf("enter your age \n");
 
-scanf("%d",&age);
+    scanf("%d",&age);
 
-switch (age)
-{
-case 3:
-    printf("The age is 3\n");     
-    break;
-    
-case 17:
+    if(age==3)
+    {
+        printf("The age is 3\n");     
+        break;
+    }
+    else if(age==2){
+        printf("The age is 17\n");     
+        break;
+    }else if(age==23){  
+        printf("The age is 23\n");     
+    }else{
+         default:
+        printf("Age is not 3,13 or 23");
+    }
 
-    printf("The age is 17\n");     
-    break;
- 
- case 23:
-  
-    printf("The age is 23\n");     
- 
-
-     default:
-    printf("Age is not 3,13 or 23");
-
-
-}
-
-return 0;
+    return 0;
 }
