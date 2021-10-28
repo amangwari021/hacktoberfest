8d90666805c987d2ce87e5d3b52a0699757c8bb4
Abhishekgwari <93308577+Abhishekgwari@users.noreply.github.com>
2021-10-28 11:49:24 +0530

    Update Tutorial7.c
    hacktoberfest-accepted

--- Tutorial7.c
+++ Tutorial7.c
@@ -24,11 +24,11 @@ int find_line() {
 int main()
 {
     int a= 8;
-  const  float b = 7.333; 
+  const  float d = 7.333; 
  // PI = 7.33; cannot do this because PI is constant
   printf("%d", PI);
-   // b = 7.22;    //cannot do this b is a constant
+   // d = 7.22;    //cannot do this b is a constant
      //printf("hello world\n"); 
-   //  printf(" The value of a is %d and the value of b is %f\n" , a , b); 
+   //  printf(" The value of a is %d and the value of d is %f\n" , a , d); 
          return 0;
 }
